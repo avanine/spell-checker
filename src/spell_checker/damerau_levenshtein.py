b12dd7dc7113588325b17c8c7bb9c562dcc55dc0
Marike <marike.puranen@laura.fi>
2026-03-26 21:13:25 +0200

    Damerau Levenshtein distance calculation

--- src/spell_checker/damerau_levenshtein.py
+++ src/spell_checker/damerau_levenshtein.py
@@ -11,8 +11,24 @@ class DamerauLevenshtein:
         return matrix
 
     def distance(self, word1, word2):
-        m = len(word1) + 1
-        n = len(word2) + 1
-        matrix = self.create_matrix(m, n)
-        
-        return 0
+        m = len(word1)
+        n = len(word2)
+        matrix = self.create_matrix(m + 1, n + 1)
+
+        # Käydään läpi matriisin jokainen solu ja lasketaan etäisyys
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                letters_distance = 0 if word1[i - 1] == word2[j - 1] else 1
+
+                matrix[i][j] = min(
+                    matrix[i - 1][j] + 1,   # Kirjaimen poistaminen (deletion)
+                    matrix[i][j - 1] + 1,   # Kirjaimen lisääminen (insertion)
+                    matrix[i - 1][j - 1] + letters_distance # Kirjaimen korvaaminen (substitution)
+                )
+
+                # Transpositio (kahden peräkkäisen merkin vaihtaminen)
+                # Jos kaksi peräkkäistä merkkiä ovat samat ensimmäisessä ja toisessa sanassa, mutta eri järjestyksessä, niin vaihto voidaan tehdä yhdellä operaatiolla kahden sijaan
+                if i > 1 and j > 1 and word1[i-1] == word2[j-2] and word1[i-2] == word2[j-1]:
+                    matrix[i][j] = min(matrix[i][j], matrix[i-2][j-2] + 1)
+
+        return matrix[m][n]
