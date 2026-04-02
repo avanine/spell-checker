c14830353a21f9bf1c51c3d84e3722e6b397168c
Marike <marike.puranen@laura.fi>
2026-04-03 00:38:56 +0300

    Sort suggestions and return max top 5

--- src/spell_checker/suggestions.py
+++ src/spell_checker/suggestions.py
@@ -4,10 +4,12 @@ from spell_checker.damerau_levenshtein import DamerauLevenshtein
 
 def suggest(trie, word, max_distance=2):
     """Vertaa annettua sanaa jokaiseen sanakirjan sanaan yksi kerrallaan.
-    Palauttaa listan sanoista, joiden etäisyys on enintään max_distance."""
+    Palauttaa enintään 5 lähintä sanaa, joiden etäisyys on enintään max_distance."""
     dl = DamerauLevenshtein()
     suggestions = []
     for trie_word in trie:
-        if dl.distance(word, trie_word) <= max_distance:
-            suggestions.append(trie_word)
-    return suggestions
+        dist = dl.distance(word, trie_word)
+        if dist <= max_distance:
+            suggestions.append((dist, trie_word))
+    suggestions.sort(key=lambda x: x[0])
+    return [s for _, s in suggestions[:5]]
